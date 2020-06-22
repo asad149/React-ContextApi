ce078313ad998a4e06f0d4bbb8652099128068bd
asad149 <asad.arshad.94849@gmail.com>
2020-06-22 20:00:45 +0500

    Developed by Context Api

---
+++ src/Child.js
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import CounterContext from "./CounterContext";
+const Child = () => {
+  let counterValue = useContext(CounterContext);
+
+  return (
+    <div>
+      <h2>This is First Child</h2>
+      <h1>The value of counter is : {counterValue[0]}</h1>
+      <button
+        onClick={() => {
+          counterValue[1](++counterValue[0]);
+        }}
+      >
+        Increment
+      </button>
+    </div>
+  );
+};
+
+export default Child;
