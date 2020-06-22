0a403edd1b28b921971d91783288be48db823015
asad149 <asad.arshad.94849@gmail.com>
2020-06-22 22:41:47 +0500

    Update Child2.js

--- src/Child2.js
+++ src/Child2.js
@@ -2,11 +2,13 @@ import React, { useReducer } from "react";
 import CounterReducer from "./CounterReducer";
 
 const Child2 = () => {
-  let [state, dispatch] = useReducer(10);
+  let [state, dispatch] = useReducer(CounterReducer(10));
 
   return (
     <div>
       <h2>The Value of the Reducer : {state}</h2>
+
+      <button onClick={() => dispatch("INCREMENT")}>Increment Reducer</button>
     </div>
   );
 };
